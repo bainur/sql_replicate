9375fec9d9f9d2f8ac388882f0c91d52a9a23ec1
inoe <mbainurb@gmail.com>
2015-05-19 13:36:21 +0700

    api result

--- lib/ncr_mapping/api_result.rb
+++ lib/ncr_mapping/api_result.rb
@@ -8,18 +8,41 @@ class NcrMapping::ApiResult
     limit_row = limit_initial(limit_export)
     puts @client
     puts "select TOP #{limit_row} * from HstvbofqAssignment where FKStoreID is not null Order by HstvbofqAssignmentID desc"
-    results = @client.execute("select TOP #{limit_row} * from HstvbofqAssignment where FKStoreID is not null Order by HstvbofqAssignmentID desc").each # do |rs|
+    results = @client.execute("select TOP #{limit_row} * from HstvbofqAssignment where FKStoreID is not null AND FKStoreID !=0 Order by ActivityDateTime desc").each # do |rs|
 
     res = []
-    results.each do |x|
-      user_id = x["FKvbofqMemberAccountID"] # this is user id from ncr
+    results.each do |rs|
+      user_id = rs["FKvbofqMemberAccountID"] # this is user id from ncr
+      assingnment_id = rs["HstvbofqAssignmentID"]
+
       card_numbers = @client.execute("select CardNumber from vbofqMemberAccount where vbofqMemberAccountID = #{user_id}").each
       a = {}
       a = {:card_number => card_numbers.first["CardNumber"]} unless card_numbers.blank?
-      res << x.merge!(a)
+
+      ## transaction detail
+
+      @client.execute("select * from HstvbofqCheck where FKHstvbofqAssignmentID
+             = #{assingnment_id}").each do |detail|
+              date_of_business = detail["DateOfBusiness"]
+              time_stamp = date_of_business.to_time
+              store_id = detail["FKStoreID"]
+              status = detail["Status"]
+              cashier = detail["FKEmployeeID"].to_s + detail["EmployeeName"].to_s
+              subtotal = detail["CheckTotal"]
+              receipt_number = detail["CheckNumber"]
+      hstv = {
+          :date_of_business => date_of_business,
+          :store_id => store_id, :status => status,
+          :cashier => cashier, :subtotal => subtotal, :receipt_number => receipt_number
+      }
+      puts hstv
+      rs.merge!(hstv)
+      end
+
+      res << rs.merge!(a)
     end
 
-    return results.to_json
+    return res.to_json
   end
 
   def self.limit_initial(limit_export)
