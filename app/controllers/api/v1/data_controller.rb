cc4daa0ba25d7b902f2ebef62b3ed87e0891258d
inoe <mbainurb@gmail.com>
2015-05-26 11:55:10 +0700

    api ncr

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -5,13 +5,15 @@ class Api::V1::DataController < ApplicationController
   end
 
   def ncr_receipt
-    result = NcrMapping::ApiResult.ncr_receipt
+    row = params[:row].blank? ? 10 : params[:row].to_i
+    result = NcrMapping::ApiResult.ncr_receipt(row)
 
     render :json => result
   end
 
   def ncr_reward_transaction
-    result = NcrMapping::ApiResult.reward_transaction
+    row = params[:row].blank? ? 10 : params[:row].to_i
+    result = NcrMapping::ApiResult.reward_transaction(row)
     render :json => result
   end
 end
